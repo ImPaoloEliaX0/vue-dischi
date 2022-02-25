9524e31ecfe1984e81335f469fbdc9d57e4b1cfb
Paolo Elia <92871203+ImPaoloEliaX0@users.noreply.github.com>
2022-02-25 11:00:06 +0100

    Esercizio funzionante

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="top-header d-flex ">
+        <img src="../assets/img/logo.png" alt="logo di spotify">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MyHeader',
+}
+</script>
+
+<style scoped lang="scss">
+    .top-header{
+        width: 100%;
+        height: 50px;
+        background-color: #2b3742;
+
+        img{
+            width: 120px;
+            margin-left: 10px;
+        }
+    }
+</style>
---
+++ src/components/MyMain.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="container">
+        <div class="row row-cols-2 row-cols-sm-3 row-cols-lg-5 justify-content-center">
+            <Album
+            v-for="(album, indice) in listaAlbum" 
+            :key="indice"
+            :album="album"
+            >
+            </Album>
+        </div>
+</div>
+</template>
+
+<script>
+    //integrazione di axios
+    const axios = require('axios');
+    import Album from './partials/Album.vue'
+
+export default {
+    name: 'MyMain',
+    data(){
+        return{
+            listaAlbum: []
+        }
+    },
+    components:{
+        Album,
+    },
+    methods:{
+        getAlbum(){
+            axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+            .then((response) => {
+                // successo
+                this.listaAlbum = response.data.response;
+                console.log(response);
+            })
+            .catch(function (error) {
+                // errore
+                console.log(error);
+            })
+            .then(function () {
+                //  eseguito
+            });
+        }
+    },
+    created(){
+        this.getAlbum();
+    }
+}
+</script>
+
+<style scoped lang="scss">
+ @import "../assets/style/generale.scss";
+  @import "../assets/style/variabili.scss";
+</style>
